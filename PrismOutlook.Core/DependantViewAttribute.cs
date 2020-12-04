3f567e4a754330574827d48644789e0d7b10c26b
Richard Holland <drdispatch@gmail.com>
2020-12-03 23:45:09 -0600

    more nav/dep view and auto expand mail list group

---
+++ PrismOutlook.Core/DependantViewAttribute.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace PrismOutlook.Core
+{
+    [AttributeUsage(AttributeTargets.Class, AllowMultiple = true)]
+    public class DependantViewAttribute : Attribute
+    {
+        
+        public string Region { get; set; }
+        public Type Type { get; set; }
+
+        public DependantViewAttribute(string region, Type type)
+        {
+            Region = region ?? throw new ArgumentNullException(nameof(region));
+            Type = type ?? throw new ArgumentNullException(nameof(type));
+        }
+
+    }
+}
